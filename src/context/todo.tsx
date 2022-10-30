5b07fa56b89916e30c588c841819884a2b28783a
Diego Acosta <diegoacosta211@gmail.com>
2022-10-30 15:32:13 -0300

    persist todos

--- src/context/todo.tsx
+++ src/context/todo.tsx
@@ -1,82 +1,101 @@
 import React, { createContext, useContext, useMemo, useState } from "react";
-import { Todo as TodoType, TodoContext as TodoContextType, Filter } from "../../types";
+import {
+  Todo as TodoType,
+  TodoContext as TodoContextType,
+  Filter,
+} from "../../types";
 
 const TodosContext = createContext<TodoContextType | null>(null);
 
-const TodosProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-
+const TodosProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [filter, setFilter] = useState<Filter | null>(null);
 
-  const [todos, setTodos] = useState<TodoType[]>([
-    {
-      id: Date.now().toString(),
-      name: "Sarasa",
-      completed: false
-    },
-  ]);
+  const [todos, setTodos] = useState<TodoType[]>(
+    JSON.parse(localStorage.getItem("todos") || "[]")
+  );
 
   const addTodo = (todo: TodoType) => {
-    setTodos(state => [todo, ...state]);
-  }
+    const draft = [todo, ...todos];
+    localStorage.setItem("todos", JSON.stringify(draft));
+    setTodos(draft);
+  };
 
   const deleteTodo = (id: string) => {
-    setTodos(state => {
-      return state.filter(todo => todo.id != id);
-    })
-  }
+    const draft = todos.filter((todo) => todo.id != id);
+    localStorage.setItem("todos", JSON.stringify(draft));
+    setTodos(draft);
+  };
 
   const toggleComplete = (id: string) => {
-    setTodos(state => {
-      return state.map(todo => {
-        if (todo.id == id) {
-          return {
-            ...todo,
-            completed: !todo.completed
-          }
-        } else {
-          return todo;
-        }
-      });
-    })
-  }
+    const draft = todos.map((todo) => {
+      if (todo.id == id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      } else {
+        return todo;
+      }
+    });
+    localStorage.setItem("todos", JSON.stringify(draft));
+    setTodos(draft);
+  };
 
   const toggleAllComplete = (completed: boolean) => {
-    setTodos(state => {
-      return state.map(todo => ({ ...todo, completed: completed}))
-    });
-  }
+    const draft = todos.map((todo) => ({ ...todo, completed: completed }));
+    localStorage.setItem("todos", JSON.stringify(draft));
+    setTodos(draft)
+  };
 
   const clearCompleted = () => {
-    setTodos(state => {
-      return state.filter( todo => !todo.completed)
-    })
-  }
+    const draft = todos.filter((todo) => !todo.completed);
+    localStorage.setItem("todos", JSON.stringify(draft));
+    setTodos(draft);
+  };
 
   const filteredTodos = useMemo(() => {
     let matches = todos;
     if (filter) {
       matches = todos.filter(filter);
     }
-    console.log(matches);
     return matches;
   }, [todos, filter]);
 
   const handleChangeFilter = (filter: Filter) => {
     setFilter(() => filter);
-  }
+  };
 
   return (
     <TodosContext.Provider
-      value={{ todos, addTodo, deleteTodo, toggleComplete, toggleAllComplete, clearCompleted, filteredTodos, handleChangeFilter }}
+      value={{
+        todos,
+        addTodo,
+        deleteTodo,
+        toggleComplete,
+        toggleAllComplete,
+        clearCompleted,
+        filteredTodos,
+        handleChangeFilter,
+      }}
     >
       {children}
     </TodosContext.Provider>
   );
-}
+};
 
 const useTodos = () => {
-  const { todos, addTodo, deleteTodo, toggleComplete, toggleAllComplete, clearCompleted, handleChangeFilter, filteredTodos } =
-    useContext(TodosContext) as TodoContextType;
+  const {
+    todos,
+    addTodo,
+    deleteTodo,
+    toggleComplete,
+    toggleAllComplete,
+    clearCompleted,
+    handleChangeFilter,
+    filteredTodos,
+  } = useContext(TodosContext) as TodoContextType;
 
   return {
     todos,
@@ -86,8 +105,8 @@ const useTodos = () => {
     toggleComplete,
     toggleAllComplete,
     clearCompleted,
-    handleChangeFilter
+    handleChangeFilter,
   };
-}
+};
 
 export { TodosProvider, useTodos };
